89fa31c126493d5ae0f1b3c341c24812645ac4b5
baizhi958216 <1475289190@qq.com>
2023-09-06 00:15:13 +0800

    添加方块和世界背景
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- lib/ember_quest.dart
+++ lib/ember_quest.dart
@@ -2,11 +2,20 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutterflamegame/actors/ember.dart';
 
+import 'actors/water_enemy.dart';
+import 'managers/segment_manager.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
+
+import 'package:flutter/material.dart';
+
 class EmberQuestGame extends FlameGame {
   EmberQuestGame();
 
   late EmberPlayer _emberPlayer;
-  
+  double objectSpeed = 0.0;
+
   final world = World();
   late final CameraComponent cameraComponent;
 
@@ -27,7 +36,46 @@ class EmberQuestGame extends FlameGame {
     cameraComponent.viewfinder.anchor = Anchor.topLeft;
     addAll([cameraComponent, world]);
 
-    _emberPlayer = EmberPlayer(position: Vector2(128, canvasSize.y - 70),);
+    initializeGame();
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+          break;
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+          break;
+        case Star:
+          break;
+        case WaterEnemy:
+          break;
+      }
+    }
+  }
+
+  void initializeGame() {
+    // Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
+
+    _emberPlayer = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 70),
+    );
     world.add(_emberPlayer);
   }
 }
